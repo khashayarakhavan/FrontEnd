16927d91e93ad856b70dd9460f9cb7e60e46f1cd
AftoflBIG5 <AftoflBIG5@gmail.com>
2020-11-13 06:48:14 -0800

    Improve Button /heroHeader.styles.js :Animate button on mobile devices :lipstick:

--- src/components/complex/hero-header/heroHeader.styles.js
+++ src/components/complex/hero-header/heroHeader.styles.js
@@ -6,6 +6,7 @@ import mixins from '../../../design/mixins.styles';
 import fonts from '../../../design/fonts.styles';
 import colors from '../../../design/colors';
 import sizes from '../../../design/sizes';
+import {bubbleGum, hoverOff} from '../../../design/motions.styles';
 import {respond} from '../../../design/responsive';
 import shadows from '../../../design/shadows.styles';
 import events from '../../../design/events.styles';
@@ -17,48 +18,38 @@ import {AnimationCreateRobot} from '../../../design/animations.styles';
 
 
 //Code
-export const Heading = styled.div`
-  ${fonts.mixins.heroHeader}
-  font-size: clamp(30px, 10vw, 60px);
-  line-height: clamp(4rem, 6vw, 7rem);
-  line-height: 6rem;
-  width: 100%;
-  color: ${colors.primary};
-  display: block;
-  position: relative;
-  ${respond.mobile.large`
-    font-size: clamp(51px, 8vw, 80px);
-  `}
-  ${respond.pc.min`
-    font-size: clamp(40px, 4vw, 100px);
-  `}
-`;
-
 
 export const HeroHeaderLeft = styled.div`
+  grid-row: 2 / span 1;
   position: relative;
+  text-rendering: optimizeLegibility;
+  overflow-wrap: break-word;
   height: 50vh;
   max-width: 100%;
-  padding-top: 15vh;
-  padding-left: 2rem;
-  ${"" /* overflow: hidden !important; */}
   z-index: 100;
-  background: blue;
+  ${'' /* background: blue; */}
   grid-column: center-start / center-end;
-  grid-row: 2 / 3;
+  display: grid;
+  grid-template-rows: min-content min-content minmax(min-content, 1fr);
+  margin-top: clamp(50px, 16.6666vw, 16.6666vw);
+
+  ${'' /* align-items: center; */}
 
   ${respond.mobile.large`
-    grid-row: 3 / 4;
+    height: 45vh;
+     
+  `}
+  ${respond.mobile.max`
+    
   `}
   ${respond.pc.min`
     grid-column: center-start / col-end 4;
-    grid-row: 2 / 3;
-    height: 70vh;
-
+    height: 90vh;
+  `}
+  ${respond.tablet.large`
+    grid-column: center-start / center-end !important;
   `}
 `;
-
-
 
 
 
@@ -68,14 +59,14 @@ export const HeroHeaderLeft = styled.div`
 
 export const HeroHeaderRight = styled.div`
   grid-column: center-start / center-end;
-  grid-row: 1 / 2;
-  height: min-content;
+  grid-row: 3 / span 1;
+  height: 50vh;
   position: relative;
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: 1fr;
   overflow: hidden !important;
-  background: red;
+  ${'' /* background: red; */}
 
   & > * {
     justify-self: flex-start;
@@ -87,30 +78,86 @@ export const HeroHeaderRight = styled.div`
   }
 
   ${respond.mobile.large`
-    height: min-content;
-    grid-row: 2/3;
+    height: 45vh;
     padding-top: 0vh;
   `}
+  ${respond.tablet.large`
+    grid-column: center-start / center-end !important;
+  `}
   ${respond.pc.min`
-    grid-row: 2 / 3;
+    grid-row: 2 / span 1;
     grid-column: col-start 5 / center-end;
-    height: 70vh;
+    height: 90vh;
     padding-top: 0vh;   
   `}
 `;
+
+export const Heading = styled.div`
+  ${fonts.mixins.heroHeader}
+  color: ${colors.secondary.darkest};
+  letter-spacing: 0px;
+  font-style: normal;
+  overflow-wrap: break-word;
+  word-break: break-word;
+  text-rendering: optimizeLegibility;
+  
+  padding-top: clamp(6rem, 20vw, 20vw);
+  padding-left: clamp(3rem, 10vw, 10vw);
+  font-size: clamp(2.5rem, 8.3vw, 8.3vw);
+  line-height: clamp(3rem, 10vw, 10vw);
+  width: 100%;
+  display: block;
+  position: relative;
+
+  ${respond.mobile.large`
+    padding-top: clamp(5.4rem, 10vw, 10vw);   
+  `}
+  ${respond.mobile.max`
+    padding-left: 0; 
+  `}
+  ${respond.pc.min`
+    font-size: clamp(4rem, 4vw, 4vw);
+    line-height: clamp(5rem, 5vw, 5vw);
+  `}
+`;
+
+export const TEXT = styled.span`
+  display: block;
+  ${'' /* height: clamp(40px, 6.666vw, 16.666vw); */}
+  ${fonts.mixins.text};
+  width: 100%;
+  
+  color: ${colors.neutrals.darkest};
+  letter-spacing: 0px;
+  font-style: normal;
+  overflow-wrap: break-word;
+  word-break: break-word;
+  text-rendering: optimizeLegibility;
+  line-height: ${sizes.lineHeight.large};
+  font-family: "Lato", sans-serif;
+  font-family: "Open Sans", sans-serif;
+  font-family: "Roboto Condensed", sans-serif;
+  font-family: "Open Sans Condensed", sans-serif;
+  font-family: "EB Garamond", serif;
+  font-family: "Cabin", sans-serif;
+  font-family: "Roboto Slab", serif;
+  font-family: "Poppins", sans-serif;
+
+  font-weight: 200;
+  font-size: clamp(10px, 3.333vw, 3.333vw);
+  padding-left: clamp(32px, 10.666vw, 10.666vw);
+
+  ${respond.mobile.max`
+    padding-left: clamp(3px, 0.5vw, 0.5vw);
+  `}
+`;
+
 
 export const HeroHeaderLottie = styled(Lottie)`
   background-color: lightgreen;
 `;
 
-export const TEXT = styled.span`
-  ${fonts.mixins.text};
-  color: ${colors.primary};
-  max-width: 95%;
-  margin-left: ${sizes.margin.smaller};
-  line-height: ${sizes.lineHeight.large} ;
-  display:block;
-`;
+
 export const Behind = styled.span`
   z-index: 5 !important;
   position: relative;
@@ -129,9 +176,10 @@ export const Front = styled.span`
 export const Skill_1 = styled.span`
   ${events.mixins.onHover.palePink};
   ${mixins.flex.inlineCenter};
+  flex: 0 0 20%;
   animation: ${background_change(colors.palePink)} 3s 2s infinite;
-  width: 60px;
-  height: 50px;
+  ${'' /* width: 60px;
+  height: 50px; */}
 
   &:hover {
     background-color: ${colors.palePink};
@@ -141,9 +189,10 @@ export const Skill_1 = styled.span`
 export const Skill_2 = styled.span`
   ${events.mixins.onHover.palePink};
   ${mixins.flex.inlineCenter};
+  flex: 0 0 25%;
   animation: ${background_change(colors.paleYellow)} 3s 3.5s infinite;
-  width: 60px;
-  height: 50px;
+  ${"" /* width: 60px;
+  height: 50px; */}
 
   &:hover {
     background-color: ${colors.paleYellow};
@@ -152,9 +201,10 @@ export const Skill_2 = styled.span`
 export const Skill_3 = styled.span`
   ${events.mixins.onHover.palePink};
   ${mixins.flex.inlineCenter};
+  flex: 0 0 19.75%;
   animation: ${background_change(colors.paleBlue)} 3s 5s infinite;
-  width: 60px;
-  height: 50px;
+  ${"" /* width: 60px;
+  height: 50px; */}
 
   &:hover {
     background-color: ${colors.paleBlue};
@@ -163,58 +213,126 @@ export const Skill_3 = styled.span`
 export const SkillTotal = styled.div`
   display: flex;
   flex-wrap: nowrap;
+  width: 70.5%;
   justify-content: flex-start;
+  justify-content: space-between;
   align-items: center;
-
+  ${"" /* width: 200px !important; */}
 `;
 
+
+export const Figure = styled.div`
+  animation: ${hoverOff} 2.5s linear;
+  position: absolute;
+  transition: all 2.5s linear;
+  @media (hover: none) {
+    animation: ${bubbleGum} 2.5s infinite;
+  };
+  width: 100%;
+  height: 100%;
+
+  z-index: -1;
+  background-color: ${colors.accent.lightest};
+  ${"" /* background-color: red; */}
+  ${"" /* z-index: 100000; */}
+  border-top-right-radius: 130%;
+  border-bottom-right-radius: 150%;
+  border-top-left-radius: 180%;
+  border-bottom-left-radius: 140%;
+  ${"" /* ${shadows.mixins.neumorphic.original}; */}
+
+  ${"" /* will-change: border-top-right-radius, border-bottom-right-radius,
+    border-top-left-radius, border-bottom-left-radius; */}
+   
+    ${"" /* transition: all 3.4s; */}
+  ${"" /* ${shadows.mixins.neumorphic.original}; */}
+`;
+
+
 export const Button = styled.div`
-  align-self: flex-start;
+  margin-top: 1.5rem;
+  transition: all 2.5s ease-out;
+  border: none;
+  justify-self: flex-start;
+  align-self: center;
+  margin-left: clamp(3.2rem, 10.666vw, 10.666vw);
+  ${"" /* background-color: ${colors.accent.lightest}; */}
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  letter-spacing: 0px;
+  font-style: normal;
+  font-size: clamp(0.9rem, 3vw, 3vw);
+  overflow-wrap: break-word;
+  word-break: break-word;
+  text-rendering: optimizeLegibility;
+  position: relative;
+  width: 46%;
+  height: clamp(5.6rem, 18.666vw, 18.666vw);
+  border-top-right-radius: 130%;
+  border-bottom-right-radius: 150%;
+  border-top-left-radius: 180%;
+  border-bottom-left-radius: 140%;
+  font-family: "Poppins", sans-serif;
+  font-weight: 200;
+  cursor: pointer;
+  align-items: flex-start;
+  align-items: center;
+  padding-left: 1vw;
+  ${"" /* background-color: ${colors.accent.lightest}; */}
+
+  will-change: border-radius;
+
+  ${respond.mobile.max`
+    margin-left: clamp(3px, 0.5vw, 0.5vw);
+  `}
+  span {
+    font-family: "Rubik Mono One", Cambria, "Times New Roman", Times, sans-serif;
+    font-weight: 400;
+    color: ${colors.secondary.lighter};
+    color: ${colors.shadows.light};
+    color: ${colors.neutrals.light};
+  }
 
   &,
   &:link,
   &:visited {
-    ${fonts.mixins.CTA.visitProfile}};
-    display: block;
-    margin-bottom: 10rem !important;
-    margin-right: 2rem !important;
-    margin-block-end: 2rem !important;
+    color: ${colors.neutrals.lighter};
     text-transform: uppercase;
     text-decoration: none;
-    padding: 0rem 3rem;
-    display: inline-block;
-    border-radius: 1rem;
-    transition: all 0.4s;
-    position: relative;
-    border: none;
-    cursor: pointer;
-    color: #094e7c;
-    ${shadows.mixins.neumorphic.original};
   }
-
+  &::after {
+    content: "";
+    display: inline-block;
+    height: 100%;
+    width: 100%;
+    border-radius: 10px;
+    ${"" /* background-color: ${colors.accent.lightest}; */}
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1000;
+    ${"" /* opacity: 0; */}
+  }
   &:hover {
-    transform: translateY(-8px);
-    background-color:  #F8F8F9;
-    box-shadow:  4px 4px 15px 2px rgba(125, 126, 127, 0.42),
-           -3px -3px 12px 2px rgba(255, 255, 255, 0.93); 
+    background: none;
 
-  &:hover::after {
-    transform: scaleX(1.4) scaleY(1.6);
-    opacity: 0;
+    & ${Figure} {
+      transition: height 2.5s ease-out;
+      ${'' /* background-color: ${colors.accent.lightest}; */}
+      animation: ${bubbleGum} 2.5s infinite linear;
+    }
   }
 
   &:active {
     outline: none;
-    background-color: #094e7c;
-    color: rgba(252, 253, 253, 0.87);
-    transform: translateY(-2px);
-    box-shadow:  4px 4px 15px 2px rgba(125, 126, 127, 0.42),
-           -3px -3px 12px 2px rgba(255, 255, 255, 0.93);
+    background-color: ${colors.accent.darkest};
+    ${"" /* color: rgba(252, 253, 253, 0.87); */}
+    transform: translateY(5px);
+    ${"" /* box-shadow: 4px 4px 15px 2px rgba(125, 126, 127, 0.42),
+      -3px -3px 12px 2px rgba(255, 255, 255, 0.93); */}
   }
 `;
-
-
-
 
 
 export const HeroHeaderContainer = styled.header`
--- src/design/motions.styles.jsx
+++ src/design/motions.styles.jsx
@@ -1,4 +1,5 @@
 import styled, { keyframes } from "styled-components";
+import colors from '../design/colors';
 
 
 export const background_change = (backColor) => keyframes`
@@ -35,6 +36,94 @@ export const neon = keyframes`
   100% {
     color: #6eeb83;
   }
+`;
+
+export const hoverOff = keyframes`
+100%{
+  height: clamp(5.6rem, 18.333vw, 18.333vw);
+    
+    border-top-right-radius: 130%;
+  border-bottom-right-radius: 150%;
+  border-top-left-radius: 180%;
+  border-bottom-left-radius: 140%;
+}
+`;
+
+export const bubbleGum = keyframes`
+  
+  0%{
+    height: clamp(5.6rem, 18.333vw, 18.333vw);
+    
+    border-top-right-radius: 130%;
+    border-bottom-right-radius: 150%;
+    border-top-left-radius: 180%;
+    border-bottom-left-radius: 140%;
+  }
+  
+  
+  15%{
+    height: clamp(6.5rem, 21vw, 21vw);
+
+
+    border-top-right-radius: 120%;
+    border-top-left-radius: 135%;
+    border-bottom-left-radius: 115%;
+    border-bottom-right-radius: 150%;
+
+
+  }
+  
+  30%{
+    height: clamp(6rem, 20vw, 20vw);
+    border-top-right-radius: 130%;
+    border-top-left-radius: 195%;
+    border-bottom-left-radius: 145%;
+    border-bottom-right-radius: 200%;
+  }
+
+  
+  45% {
+    height: clamp(6.1rem, 21vw, 21vw);
+    border-top-right-radius: 135%;
+    border-top-left-radius: 145%;  
+    border-bottom-left-radius: 165%;
+    border-bottom-right-radius: 180%;
+
+  }
+
+  60% {
+    height: clamp(6.9rem, 23vw, 23vw);
+
+    border-top-right-radius: 160%;
+    border-bottom-right-radius: 155%;
+    border-top-left-radius: 180%;
+    border-bottom-left-radius: 140%;
+  }
+
+  75% {
+    height: clamp(7.5rem, 25vw, 25vw);
+
+    border-top-left-radius: 180%;
+    border-top-right-radius: 160%;
+    border-bottom-right-radius: 155%;
+    border-bottom-left-radius: 140%;
+    
+    
+  }
+
+  100%{
+    height: clamp(5.6rem, 18.333vw, 18.333vw);
+
+
+    border-top-right-radius: 130%;
+    border-bottom-right-radius: 150%;
+    border-top-left-radius: 180%;
+    border-bottom-left-radius: 140%;
+
+    
+    
+  }
+  
 `;
 
 
